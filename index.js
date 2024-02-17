f2814bc0abccf06f2ef899d6f25de4ad1ff36dfe
alexander perlov <perlov3301@gmal.com>
2024-02-17 19:45:05 +0200

    dynamicly defined file from res.url to be read

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import http from 'node:http';
 import path from 'node:path';
 import fs from 'node:fs';
 import { fileURLToPath } from 'node:url'; 
+
+ function myDir() {
+    const myurl = import.meta.url;
+    const mypath = fileURLToPath(myurl);
+    const mydir = path.dirname(mypath);
+    return mydir;
+ }
  function myPublic () {
     const myurl = import.meta.url;
     const mypath = fileURLToPath(myurl);
@@ -10,46 +17,75 @@ import { fileURLToPath } from 'node:url';
     const pathtext = path.join(mydir, 'public');
      return pathtext;
  }
+ function myTime () {
+    const myT = new Date();
+    const myH = myT.getHours();
+    const myM = myT.getMinutes();
+    let myTt = "";
+    if (myM<9) { myTt= `${myH}:0${myM}`; }
+    else { myTt= `${myH}:${myM}`; }
+    return myTt;
+ }
  function myTtext (a) {
     const myT = new Date();
     const myH = myT.getHours();
     const myM = myT.getMinutes();
     let myTt = "";
-    if(myM>9) { myTt = `${myH}:${myM}`; }
-    else { myTt = `${myH}:0${myM}`; }
+    if(myM<9) { myTt = `${myH}:0${myM}`; }
+    else { myTt = `${myH}:${myM}`; }
     const myCss = 'style="text-align:center"';
     return `<h1 ${myCss}>${a} ${myTt}</h1>`;
 };
 const server = http.createServer((req,res)=>{
-    // console.log(req.url);
-    if(req.url==='/') {
-        const myp = myPublic();
-        console.log(`myp dir: ${myp}`);
-        fs.readFile(path.join(myp, 'index.html'), (err, content)=>{
-            if (err) throw err;
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content);
-        });
-    } 
-    if (req.url==='/api/users') {
-        const users = [
-            { name: 'Bob Smith', age:40 },
-            { name: 'Mary Doe', age:33 },
-        ];
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(users));
+    // build file path 
+    const myp = myPublic();
+    let myu = req.url === '/' ? 'index.html':req.url;
+    let filePath = path.join(myp,myu); // public/index.html
+    // extension of file
+    let myExtName = path.extname(filePath);
+    //content-type : text/html application/json text/css
+    let contentType = 'text/html';//default
+    // check ext and set content type
+    switch(myExtName) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
     }
-    // if(req.url==='/about') {
-    //     const myp = myPublic();
-    //     console.log(`myp dir: ${myp}`);
-    //     fs.readFile(path.join(myp, 'about.html'), (err, content)=>{
-    //         if (err) throw err;
-    //         res.writeHead(200, { 'Content-Type': 'text/html' });
-    //         res.end(content);
-    //     });
-    // } 
-    
-    
+    // read file
+    fs.readFile(filePath,'utf8', (err, content)=>{
+        if(err) { // errno code syscall path
+            //  console.log( `keys of err: ${Object.keys(err)}`); 
+            if (err.code==='ENOENT') { // not found error
+              //  const myp = myPublic();
+                fs.readFile(path.join(myDir(), 'public','404.html'), (err, content)=>{
+                    if (err) throw err;
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content,'utf8');
+                });
+              } // err.code==='enoent'
+              else {//some server error
+                res.writeHead(500, {'Content-Type':'text/html'});
+                res.end(myTtext(`server error: ${err.code}`));
+              }
+            } // if err
+        else { // success
+          console.log(`file ${filePath} was read ${myTime()}`);
+          res.writeHead(200, {'Content-Type': contentType});
+          res.end(content, 'utf8');
+        }
+    });
 });
 // server.listen(5000, ()=>console.log('Server is running on 5000'));
 const PORT = process.env.PORT || 5000;
